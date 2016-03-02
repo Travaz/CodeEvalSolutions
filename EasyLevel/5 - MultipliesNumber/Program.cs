25ec87d64a233717ab6614b0383055a10df68a82
daniel.travaglia <daniel.travaglia@marconirovereto.it>
2016-03-02 19:01:23 +0100

    Aggiunta soluzione Multiplies of a Number (incompleta)

---
+++ EasyLevel/5 - MultipliesNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace _5___MultipliesNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = args.Length > 0 ? args[0] : "input.txt";
+            using (StreamReader reader = File.OpenText(input))
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    string[] splitted = line.Split(',');
+
+                    int number = int.Parse(splitted[0]);
+                    int multiplier = int.Parse(splitted[1]);
+                    int multipliering = multiplier;
+
+                    if (multipliering >= number)
+                        Console.WriteLine(multipliering);
+                    else 
+                        while(multipliering <= number)
+                        {   
+                            multipliering *= 2;
+                            if (multipliering >= number)
+                                Console.WriteLine(multipliering);                                   
+                    }
+                }
+        }
+    }
+}
