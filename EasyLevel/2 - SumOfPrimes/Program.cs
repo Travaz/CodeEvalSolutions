f733104be6222e73964b2db2f05f9df8ee291454
daniel.travaglia <daniel.travaglia@marconirovereto.it>
2016-03-02 17:39:52 +0100

    Aggiunte soluzioni 1 e 11

---
+++ EasyLevel/2 - SumOfPrimes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2___SumOfPrimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Primes = PrimesLessThanThousend();
+            Console.WriteLine(Primes.Sum());
+            Console.ReadLine();
+        }
+
+        static List<int> PrimesLessThanThousend()
+        {
+            int divisore;
+            bool primo;
+            List<int> Primes = new List<int>();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                primo = true;
+                divisore = 2;
+                while (divisore < i)
+                {
+                    if (i % divisore == 0)
+                    {
+                        primo = false;
+                        divisore = i;
+                    }
+                    divisore++;
+                }
+                if (primo == true)
+                    Primes.Add(i);
+            }
+            return Primes;
+        }
+    }
+}
