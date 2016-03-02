d4acce1dfd31b5624038f87f036a964e06a7dcd6
daniel.travaglia <daniel.travaglia@marconirovereto.it>
2016-03-02 18:37:04 +0100

    Aggiunta soluzione Reverse Word

---
+++ EasyLevel/4 - Reverse Word/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4___Reverse_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = args.Length > 0 ? args[0] : "input.txt";
+            using (StreamReader reader = File.OpenText(input))
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    string[] splitted = line.Split(' ');
+
+                    for (int i = splitted.Length - 1; i >= 0; i--)
+                    {
+                        Console.Write(splitted[i] + " ");
+                    }
+                    Console.Write("\n");                  
+                }
+        }
+    }
+}
