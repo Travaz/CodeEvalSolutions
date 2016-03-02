b0484187e929f5338ab08510fe74c99139246117
daniel.travaglia <daniel.travaglia@marconirovereto.it>
2016-03-02 20:39:35 +0100

    Aggiunta soluzione SumsOfInteger

---
+++ EasyLevel/10 - SumsOfIntegers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10___SumsOfIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = args.Length > 0 ? args[0] : "input.txt";
+            Console.WriteLine(File.ReadAllLines(input)
+                .Select(line =>
+                {
+                    return int.Parse(line);
+                })
+                .ToList().Sum());               
+        }
+    }
+}
+
