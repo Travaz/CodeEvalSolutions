4d1a2389a85312fc649b425f8db588edc69ecfb4
daniel.travaglia <daniel.travaglia@marconirovereto.it>
2016-03-02 19:10:10 +0100

    Aggiunta soluzione File Size

---
+++ EasyLevel/FileSize/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FileSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = args.Length > 0 ? args[0] : "input.txt";
+            FileInfo f = new FileInfo(input);
+            Console.WriteLine(f.Length);
+            Console.ReadLine();
+        }
+    }
+}
