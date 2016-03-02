bb04f66594c49d4bb05ed93b4051025e080b1364
daniel.travaglia <daniel.travaglia@marconirovereto.it>
2016-03-02 17:38:45 +0100

    Aggiunte soluzioni problema 1 (PrimiPolindromi) e problema 11(OddNumbers)

--- EasyLevel/1 - PrimePalindrome/Program.cs
+++ EasyLevel/1 - PrimePalindrome/Program.cs
@@ -10,10 +10,35 @@ namespace _1___PrimePalindrome
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < length; i++)
+            List<int> Primes = PrimesLessThanThousend();
+            Console.WriteLine(Primes.Max());
+            Console.ReadLine();
+        }
+
+        static List<int> PrimesLessThanThousend()
+        {
+            int divisore;
+            bool primo;
+            List<int> Primes = new List<int>();
+
+            for (int i = 900; i < 1000; i++)
             {
+                primo = true;
+                divisore = 2;
+                while (divisore < i)
+                {
+                    if (i % divisore == 0)
+                    {
+                        primo = false;
+                        divisore = i;
+                    }           
+                    divisore++;
+                }
+                if (primo == true && i == 929)
+                    Primes.Add(i);
 
             }
+            return Primes;
         }
     }
 }
---
+++ EasyLevel/11 - OddNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11___OddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> oddNumbers = OddNumbers();
+            foreach (var item in oddNumbers)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+
+        static List<int> OddNumbers()
+        {
+            List<int> oddNumbers = new List<int>();
+            for (int i = 1; i < 100; i++)
+            {
+                if (!(i % 2 == 0))
+                    oddNumbers.Add(i);                   
+            }
+            return oddNumbers;
+        }
+    }
+}
