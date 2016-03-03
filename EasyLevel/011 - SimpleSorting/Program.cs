9a018c2fb5b70d1a936d7190e3cc4e9dbf1bb79f
daniel.travaglia <daniel.travaglia@marconirovereto.it>
2016-03-03 17:09:17 +0100

    Aggiunta soluzione simpleSorting (incompleta)

---
+++ EasyLevel/011 - SimpleSorting/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace _011___SimpleSorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = args.Length > 0 ? args[0] : "input.txt";
+            using (StreamReader reader = File.OpenText(input))
+            {
+                //First try
+
+                while (!reader.EndOfStream)
+                {
+                    List<double> doubleNumbers = new List<double>();
+                    string line = reader.ReadLine();
+
+                    foreach (string DoubleAsString in line.Split(' '))
+                        doubleNumbers.Add(Double.Parse(DoubleAsString));
+
+                    doubleNumbers.Sort();
+                    foreach (var number in doubleNumbers)
+                    {
+                        Console.Write($"{number} ");
+                    }
+                    Console.WriteLine();
+                }
+
+                //Second Try
+
+                //while (!reader.EndOfStream)
+                //{
+                //    List<double> doubles = reader.ReadLine().Split(' ').Select(s => double.Parse(s)).ToList();
+                //    foreach (var item in doubles)
+                //    {
+                //        Console.WriteLine(item);
+                //    }
+                //}
+            }
+                         
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EasyLevel/11 - SimpleSorting/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11___SimpleSorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = args.Length > 0 ? args[0] : "input.txt";
+            using (StreamReader sr = File.OpenText(input))
+            {
+                while (!sr.EndOfStream)
+                {
+                    List<double> sorted = new List<double>();
+                    string line = sr.ReadLine();
+                    var splitted = line.Split(' ');
+                    foreach (var item in splitted)
+                    {
+                        sorted.Add(double.Parse(item, System.Globalization.NumberStyles.AllowThousands));
+                    }
+                    sorted.Sort();
+                    Console.WriteLine(sorted);
+                }               
+            }
+
+        }
+    }
+}
+
