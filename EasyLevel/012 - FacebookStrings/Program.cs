75b2f47e8bdc88e22e96ba63c42d30bf109d636b
daniel.travaglia <daniel.travaglia@marconirovereto.it>
2016-03-10 18:10:22 +0100

    Aggiunta soluzione parziale Facebook Problem

--- EasyLevel/012 - FacebookStrings/Program.cs
+++ EasyLevel/012 - FacebookStrings/Program.cs
@@ -9,9 +9,42 @@ namespace _012___FacebookStrings
 {
     class Program
     {
+        static IDictionary<char, int> Dict;
+
         static void Main(string[] args)
         {
-            //To be completed
+            var input = args.Length > 0 ? args[0] : "input.txt";
+            File.ReadAllLines(input)
+                .Select(line => BeautifulString(line.Split(' ')))
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+
+        private static int BeautifulString(string[] words)
+        {
+            int value = 26;
+            int sum = 0;
+            Dict = new Dictionary<char, int>();
+
+            foreach (string word in words)
+            {              
+                for (int i = 0; i < word.Length; i++)
+                {
+                    char letter = char.ToLower(word[i]);
+                    if(char.IsLetter(letter))
+                    {
+                        if (!Dict.ContainsKey(letter))
+                            Dict.Add(letter, 1);
+                        else
+                            Dict[letter]++;
+                    }
+           
+                }               
+            }
+
+            foreach (KeyValuePair<char, int> item in Dict.OrderByDescending(key => key.Key))
+                sum += (item.Value * value--);
+            return sum;
         }
     }
 }
