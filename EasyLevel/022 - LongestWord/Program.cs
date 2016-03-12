87e3d0e81434b8d1abb3538c31e04436897cf8ae
daniel.travaglia <daniel.travaglia@marconirovereto.it>
2016-03-12 18:06:36 +0100

    Aggiunta soluzione Longest Word

---
+++ EasyLevel/022 - LongestWord/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace _022___LongestWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = args.Length > 0 ? args[0] : "input.txt";
+            File.ReadAllLines(input)
+                .Select(line => line.Split(' ')
+                    .ToList()
+                    .MaxStringLength(word => word.Length)                 
+                )
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+
+    static class MaxStringLengthExtension
+    {
+        public static string MaxStringLength<T>(this List<T> sequence, Func<T, int> function)
+        {
+            string maxItem = sequence.First().ToString();
+            foreach (var i in sequence)
+            {
+                string checkingMax = i.ToString();
+                if (checkingMax.Length > maxItem.Length)
+                    maxItem = checkingMax;
+            }
+
+            return maxItem;
+        }
+    }
+}
