26d3de8b75a4951920f03b1055e78b4e92aad744
daniel.travaglia <daniel.travaglia@marconirovereto.it>
2016-03-12 15:39:04 +0100

    Aggiunta soluzione Capitalize Word

---
+++ EasyLevel/020 - CapitalizeWords/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace _020___CapitalizeWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = args.Length > 0 ? args[0] : "input.txt";
+            using (StreamReader reader = File.OpenText(input))
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    var splitted = line.Split(' ');
+
+                    for (int i = 0; i < splitted.Length; i++)
+                    {
+                        string word = splitted[i];
+                        string sub = word.Substring(1);
+                        Console.Write(char.ToUpper(word[0]) + sub + " ");
+                    }
+                    Console.Write("\n");
+                }
+            Console.ReadLine();
+        }
+    }
+}
