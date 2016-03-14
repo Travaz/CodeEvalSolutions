73d33b3acd7db7b7026ebf68f9db54778e77f4ef
daniel.travaglia <daniel.travaglia@marconirovereto.it>
2016-03-14 16:44:34 +0100

    Aggiunta soluzione Penultimate Word

---
+++ EasyLevel/024 - PenultimateWord/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _024___PenultimateWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = args.Length > 0 ? args[0] : "input.txt";
+            File.ReadAllLines(input)
+                .Select(line => GetPenultimateString(line.Split(' ')))
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+
+        private static string GetPenultimateString(string[] v)
+        {
+            return v[v.Length - 2];
+        }
+    }
+}
