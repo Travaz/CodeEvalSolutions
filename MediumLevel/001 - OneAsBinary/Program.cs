c4ea95d21bd2bf77baeb428a7adee4a6869505a1
daniel.travaglia <daniel.travaglia@marconirovereto.it>
2016-03-17 11:14:32 +0100

    Aggiunto Ones in Binary Digit (Medium Level)

---
+++ MediumLevel/001 - OneAsBinary/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace _001___OneAsBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = args.Length > 0 ? args[0] : "input.txt";
+            File.ReadAllLines(input)
+                .Select(num => NumberOfOnes(num))
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+
+        public static int NumberOfOnes(string number)
+        {
+            string n = Convert.ToString(int.Parse(number), 2);
+            int count = 0;
+
+            for (int i = 0; i < n.Length; i++)
+            {
+                if (n[i] == '1')
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}
--- MediumLevel/001 - OneAsBinary/Properties/AssemblyInfo.cs
+++ MediumLevel/001 - OneAsBinary/Properties/AssemblyInfo.cs
@@ -3,34 +3,34 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // Le informazioni generali relative a un assembly sono controllate dal seguente 
-// set di attributi. Modificare i valori di questi attributi per modificare le informazioni
-// associate a un assembly.
-[assembly: AssemblyTitle("MediumLevel")]
+// set di attributi. Per modificare le informazioni associate a un assembly
+// occorre quindi modificare i valori di questi attributi.
+[assembly: AssemblyTitle("001 - OneAsBinary")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MediumLevel")]
+[assembly: AssemblyProduct("001 - OneAsBinary")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Se si imposta ComVisible su false, i tipi in questo assembly non saranno visibili 
-// ai componenti COM. Se è necessario accedere a un tipo in questo assembly da 
+// ai componenti COM.  Se è necessario accedere a un tipo in questo assembly da 
 // COM, impostare su true l'attributo ComVisible per tale tipo.
 [assembly: ComVisible(false)]
 
-// Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato come ID della libreria dei tipi
-[assembly: Guid("de1d0f9f-b6f3-44c2-907e-fa6783429175")]
+// Se il progetto viene esposto a COM, il GUID che segue verrà utilizzato per creare l'ID della libreria dei tipi
+[assembly: Guid("7c4334f4-78bb-4f3b-9348-8f33579417d2")]
 
 // Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
 //
-//      Versione principale
-//      Versione secondaria 
-//      Numero di build
+//      Numero di versione principale
+//      Numero di versione secondario 
+//      Numero build
 //      Revisione
 //
 // È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build 
-// usando l'asterisco '*' come illustrato di seguito:
+// utilizzando l'asterisco (*) come descritto di seguito:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
