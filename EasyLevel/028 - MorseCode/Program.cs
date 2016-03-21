f524664361533a7a66c9917fb32507aedc1b378d
daniel.travaglia <daniel.travaglia@marconirovereto.it>
2016-03-21 17:51:54 +0100

    Aggiunta soluzione Morse Code(lemmit)

---
+++ EasyLevel/028 - MorseCode/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace _028___MorseCode
+{
+    class Program
+    {
+        private static Dictionary<string, string> _MorseCode
+            = new Dictionary<string, string>()
+            {
+                {".-", "A"},
+                {"-...", "B"},
+                {"-.-.", "C"},
+                {"-..", "D"},
+                {".", "E"},
+                {"..-.", "F"},
+                {"--.", "G"},
+                {"....", "H"},
+                {"..", "I"},
+                {".---", "J"},
+                {"-.-", "K"},
+                {".-..", "L"},
+                {"--", "M"},
+                { "-.", "N"},
+                {"---", "O"},
+                {".--.", "P"},
+                { "--.-", "Q"},
+                {".-.", "R"},
+                {"...", "S"},
+                {"-", "T"},
+                {"..-", "U"},
+                {"...-", "V"},
+                {".--", "W"},
+                {"-..-", "X"},
+                {"-.--", "Y"},
+                {"--..", "Z"},
+
+                {".----", "1" },
+                {"..---", "2" },
+                {"...--", "3" },
+                {"....-", "4" },
+                {".....", "5" },
+                {"-....", "6" },
+                {"--...", "7" },
+                {"---..", "8" },
+                {"----.", "9" },
+                {"-----", "0" },
+
+                {"", " "},
+            };
+
+        static void Main(string[] args)
+        {
+            var input = args.Length > 0 ? args[0] : "input.txt";
+            File.ReadAllLines(input)
+                .Select(line =>
+                    line.Split(' ')
+                        .Select(_MC => _MorseCode[_MC])
+                        .Aggregate(
+                            string.Empty,
+                            (seed, str) => string.IsNullOrEmpty(seed) ? str : seed + str
+                        )
+                    )
+                    .ToList()
+                    .ForEach(Console.WriteLine);
+        }
+    }
+}
