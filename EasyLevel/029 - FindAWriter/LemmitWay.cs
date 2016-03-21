ff5f47aaa48c9d88d50f6e89621f35cc0e1c41ca
daniel.travaglia <daniel.travaglia@marconirovereto.it>
2016-03-21 18:11:04 +0100

    Aggiunta soluzione Find A Writer

---
+++ EasyLevel/029 - FindAWriter/LemmitWay.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _029___FindAWriter
+{
+    class LemmitWay
+    {
+        public static string Writer(string[] splitted)
+        {
+            var letters = splitted[0].ToCharArray();
+            var pos = splitted[1]
+                        .Substring(1)
+                        .Split(' ')
+                        .Select(p => int.Parse(p))
+                        .ToList();
+
+            return pos.Select(p => letters[p - 1])
+                      .Aggregate(
+                            string.Empty,
+                            (seed, str) => string.IsNullOrEmpty(seed) ? str.ToString() : seed + str
+                        );
+        }
+    }
+}
---
+++ EasyLevel/029 - FindAWriter/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace _029___FindAWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = (args.Length > 0) ? args[0] : "input.txt";
+            File.ReadAllLines(input)
+                .Select(line => Writer(line.Split('|')))
+                .ToList()
+                .ForEach(Console.WriteLine);
+                           
+        }
+
+        private static string Writer(string[] splitted)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var letters = splitted[0];
+            var position = splitted[1]
+                                    .Substring(1)
+                                    .Split(' ')
+                                    .Select(p => int.Parse(p))
+                                    .ToList();
+
+            for (int i = 0; i < position.Count; i++)
+            {
+                int pos = position[i] - 1;
+                sb.Append(letters[pos]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
