dd80b145d33a0e3434d58043e01ed470357f947f
daniel.travaglia <daniel.travaglia@marconirovereto.it>
2016-03-24 16:45:06 +0100

    Aggiunta soluzione Clean Words (metodo lazy)

---
+++ EasyLevel/031 - CleanWords/LemmitWay.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace _031___CleanWords
+{
+    public class LemmitWay
+    {
+        public static void test(string path)
+        {
+            File.ReadAllLines(path)
+                .Select(line =>
+                            line
+                                .SplitUsingNonLetters()
+                                .Select(word => word.ToLower())
+                                .Aggregate(string.Empty, (seed, str) => string.IsNullOrEmpty(seed) ? str : seed + " " + str)
+                        )
+                        .ToList()
+                        .ForEach(Console.WriteLine);
+        }
+    }
+
+    public static class SplittedExt
+    {
+        public static IEnumerable<string> SplitUsingNonLetters(this string str)
+        {
+            var offset = 0;
+            while (offset < str.Length)
+            {
+                while (offset < str.Length && !char.IsLetter(str[offset]))
+                {
+                    offset++;
+                }
+
+                var sb = new StringBuilder();
+
+                while (offset < str.Length && char.IsLetter(str[offset]))
+                {
+                    sb.Append(str[offset]);
+                    offset++;
+                }
+                yield return sb.ToString();
+            }
+        }
+    }
+}
--- EasyLevel/031 - CleanWords/Program.cs
+++ EasyLevel/031 - CleanWords/Program.cs
@@ -12,6 +12,31 @@ namespace _031___CleanWords
         static void Main(string[] args)
         {
             var input = args.Length > 0 ? args[0] : "input.txt";
+            File.ReadAllLines(input)
+                .Select(line => CleanUpWords(line))
+                .ToList()
+                .ForEach(Console.WriteLine);
+            Console.WriteLine();
+            LemmitWay.test(input);
+        }
+
+        //Lazy way... Don't know how to cycle until the end of the string
+        private static object CleanUpWords(string line)
+        {
+            StringBuilder sb = new StringBuilder();
+            string newline = line.Insert(line.Length, " ");
+
+            for (int i = 1; i <= newline.Length; i++)
+            {
+                char charachter = newline[i - 1];
+                if (char.IsLetter(charachter))
+                {
+                    sb.Append(charachter);
+                    if (!char.IsLetter(newline[i]))
+                        sb.Append(" ");
+                }                   
+            }
+            return sb.ToString().ToLower();
         }
     }
 }
