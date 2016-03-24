a9e86eacffdb28fac0a4eb1644a98849dcd47511
daniel.travaglia <daniel.travaglia@marconirovereto.it>
2016-03-24 12:53:54 +0100

    Aggiunta soluzione Mersenne Prime (parziale)

---
+++ EasyLevel/030 - MersennePrime/Primes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _030___MersennePrime
+{
+    class Primes
+    {
+        public double Number { get; set; }
+
+        public IEnumerable<double> CalculatePrimes()
+        {
+            for (double i = 2; i < Number; i++)
+            {
+                if (isPrime(i))
+                    yield return i;
+            }
+        }
+
+        public bool isPrime(double i)
+        {
+            for (double j = 2; j < Math.Sqrt(i); j++)
+                if (i % j == 0)
+                    return false;
+                return true;
+        }
+    }
+}
---
+++ EasyLevel/030 - MersennePrime/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _030___MersennePrime
+{
+    class Program
+    {
+        static Primes primes = new Primes();
+        static void Main(string[] args)
+        {
+            var input = args.Length > 0 ? args[0] : "input.txt";
+            File.ReadAllLines(input)
+                .Select(n => MersennePrime(double.Parse(n)))
+                .ToList()
+                .ForEach(Console.WriteLine);
+
+        }
+
+        private static string MersennePrime(double n)
+        {
+            StringBuilder sb = new StringBuilder();
+            double MarsNumber;
+            primes.Number = n;
+            
+            foreach (double number in primes.CalculatePrimes())
+            {
+                MarsNumber = Math.Pow(2, number) - 1;
+                if(MarsNumber < primes.Number)
+                    if (primes.isPrime(MarsNumber))
+                        sb.Append(MarsNumber + ", ");
+            }
+
+            return sb.ToString().TrimEnd(',', ' ');
+        }
+    }
+}
